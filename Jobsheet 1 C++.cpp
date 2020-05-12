27ca2ebbb9aae448718f02387a50d27a83f9331e
Jalaluddin Muhammad Akbar <64581904+F1B019074@users.noreply.github.com>
2020-05-12 14:06:42 +0800

    Create Jobsheet 1 C++.cpp
    Buatlah program array satu
    
    dimensi yang menampilkan
    
    NIM masing masing

---
+++ Jobsheet 1 C++.cpp
@@ -0,0 +1,40 @@
+#include <iostream> using namespace std; int main()
+{
+cout<<"Nama : Jalaluddin
+Muhammad Akbar\n";
+cout<<"Nim : F1B019074\n";
+cout<<"Kelompok: Kelompok
+15\n\n";
+int nim[14]=
+{1,12,16,18,4,6,100,20,30,40,50,19,
+15,23};
+
+cout << "Nim ke-1 adalah " <<
+nim[0] << endl;
+cout << "Nim ke-2 adalah " <<
+nim[1] << endl;
+cout << "Nim ke-3 adalah " <<
+nim[2] << endl;
+cout << "Nim ke-4 adalah " <<
+nim[3] << endl;
+cout << "Nim ke-5 adalah " <<
+nim[4] << endl;
+cout << "Nim ke-6 adalah " <<
+nim[5] << endl;
+cout << "Nim ke-7 adalah " <<
+nim[6] << endl;
+cout << "Nim ke-8 adalah " <<
+nim[7] << endl;
+cout << "Nim ke-9 adalah " <<
+nim[8] << endl;
+cout << "Nim ke-10 adalah " <<
+nim[9] << endl;
+cout << "Nim ke-11 adalah " <<
+nim[10] << endl;
+cout << "Nim ke-12 adalah " <<
+nim[11] << endl;
+cout << "Nim ke-13 adalah " <<
+nim[12] << endl;
+cout << "Nim ke-14 adalah " <<
+nim[13] << endl;
+}
